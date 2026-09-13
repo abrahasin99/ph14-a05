b9e3402b2c0d779a608c9162ad04d1afa6e92012
abrahasin99 <abu.raihan.tashin@g.bracu.ac.bd>
2026-09-13 18:32:38 +0600

    fix: Add one gradient theme

--- src/components/Technology.tsx
+++ src/components/Technology.tsx
@@ -14,7 +14,7 @@ const Technology = ({ technologyPromise }: TechnologyProps) => {
     <div className="px-4 sm:px-6 md:px-12 lg:px-22">
       <h2 className="text-2xl font-extrabold sm:text-3xl lg:text-[36px]">
         Explore the{" "}
-        <span className="bg-gradient-to-r from-[#EC4899] to-[#8B5CF6] bg-clip-text text-transparent">
+        <span className="bg-brand bg-clip-text text-transparent">
           Technologies
         </span>
       </h2>
