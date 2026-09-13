501b5743677f35ea6309d2a83439db59de155fff
abrahasin99 <abu.raihan.tashin@g.bracu.ac.bd>
2026-09-13 18:26:17 +0600

    fix: Loading fallback message

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,34 @@
-import Nav from "./components/Nav"
-import Banner from "./components/Banner"
+import Nav from "./components/Nav";
+import Banner from "./components/Banner";
 import Technology from "./components/Technology";
 import { Suspense } from "react";
 import Footer from "./components/Footer";
-import type {ItechnologyType} from "./types/technologyType"
+import type { ItechnologyType } from "./types/technologyType";
 
-const technologyFetch = async():Promise<ItechnologyType[]> => {
+const technologyFetch = async (): Promise<ItechnologyType[]> => {
   const res = await fetch("/data.json");
   const data = await res.json();
   return data;
 };
 
 function App() {
-  
-  const technologyPromise=technologyFetch();
+  const technologyPromise = technologyFetch();
   return (
     <>
-      <Nav/>
-      <Banner/>
-      <Suspense fallback={<h2>Loading...</h2>}>
-        <Technology technologyPromise={technologyPromise}/>
+      <Nav />
+      <Banner />
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center py-20">
+            <p className="text-xl text-slate-500">Loading technologies...</p>
+          </div>
+        }
+      >
+        <Technology technologyPromise={technologyPromise} />
       </Suspense>
-      <Footer/>
+      <Footer />
     </>
   );
 }
 
-export default App
+export default App;
